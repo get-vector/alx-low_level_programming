838b9e0f674c5fbe59fd362ea08cf948b6f36a12
victor seyi <tayeski11@gmail.com>
2022-06-04 14:09:56 -0700

    j

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*main - entry
+*Description:putchars
+*Return:gets 0
+*/
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	for (i ='A', i <= 'Z', i++)
+		putchar(i)	
+	putchar('\n');
+	return (0);
+}
