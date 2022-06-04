9ba807c1fd411249535070a4e3e7154a16004a0d
victor seyi <tayeski11@gmail.com>
2022-06-04 15:40:43 -0700

    y

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*main - entry
+*Description:putchars
+*Return:gets 0
+*/
+
+int main(void)
+{
+	short i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+		if (i != '9')
+			putchar('\n');
+	putchar('\n');
+	return (0);
+}
