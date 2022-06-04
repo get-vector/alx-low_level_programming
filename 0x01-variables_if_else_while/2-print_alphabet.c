f52177fbdb1e9cae324b979b22756301ec08c821
victor seyi <tayeski11@gmail.com>
2022-06-04 13:48:13 -0700

    yy

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+*main - entry
+*Description:putchars
+*Return:gets 0
+*/
+
+int main(void)
+{
+	for(char i = 56; i < (56 + 26); i++)
+		putchar(i);
+	putchar("/n");
+	return (0);
+}
