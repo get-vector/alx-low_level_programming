9db04a3eb4f37cfd731a1d1069eb5c0817893fad
victor seyi <tayeski11@gmail.com>
2022-06-05 14:43:09 -0700

    as

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+*main - entry
+*Description:putchars
+*Return:gets 0
+*/
+
+int main(void)
+{
+	short i, j, k;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = i + 1; j <= '9'; j++)
+		{
+			for (k = j + 1; k <= '9'; k++)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i != '7' || j != '8'|| k != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
