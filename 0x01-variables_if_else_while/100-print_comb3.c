fc4eb822bb413d72e9ea86f4c4f1c676e72985d9
victor seyi <tayeski11@gmail.com>
2022-06-05 14:14:31 -0700

    2char

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+*main - entry
+*Description:putchars
+*Return:gets 0
+*/
+
+int main(void)
+{
+	short i, j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = i + 1; j <= '9'; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if (i != '9' && j != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
