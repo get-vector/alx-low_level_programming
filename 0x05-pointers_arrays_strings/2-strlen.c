0892077a2ec01c1975d6cf9304d6bccf1ba4ee68
victor seyi <tayeski11@gmail.com>
2022-06-14 15:32:02 -0700

    hhh

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include <string.h>
+
+/**
+ * _strlen - get length of string
+ * @s: the string
+ * Description: get string length
+ * Return: String length
+ */
+
+int _strlen(char *s)
+{
+	return strlen(s);
+}
