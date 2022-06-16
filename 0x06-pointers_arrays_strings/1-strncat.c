69edcea8e8e5fa8d37efb1757424cadf92346dac
victor seyi <tayeski11@gmail.com>
2022-06-16 03:38:07 -0700

    yu

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,10 +8,10 @@
  *  @n: no of byte
  *  Description: Join string together
  *  Return: Get another string as output
- */
+*/
 
 char *_strncat(char *dest, char *src, int n)
 {
 	char *p = strncat(dest, src, n);
-	return (p);
+	return (dest);
 }
