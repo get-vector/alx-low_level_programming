d153ba222702d73b7e128fb8e0f633ba5fb15837
victor seyi <tayeski11@gmail.com>
2022-06-16 03:03:14 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include <string.h>
+
+/**
+ *  _strcat - a concentation of string
+ *  @:
+ *  @:
+ *  Description: Join string together
+ *  Return: Get another string as output
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest,src));
+}
