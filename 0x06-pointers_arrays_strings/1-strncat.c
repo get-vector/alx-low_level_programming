ab1bee2997ffb28a78c9408e4325fff974bc424c
victor seyi <tayeski11@gmail.com>
2022-06-16 03:14:16 -0700

    66

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,14 @@
+#include <string.h>
+
+/**
+ *  _strncat - a concentation of string
+ *  @:
+ *  @:
+ *  Description: Join string together
+ *  Return: Get another string as output
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	return (strncat(dest, src, n));
+}
