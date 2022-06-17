11ed118257300c3bf51918cbab0e4d473f4eaf46
victor seyi <tayeski11@gmail.com>
2022-06-17 09:19:22 -0700

    99

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+/**
+ * reverse_array - reverse array
+ * @a: array
+ * @n: arraay size
+ * Description: For reversing array
+ */
+void reverse_array(int *a, int n)
+{
+	int q[n];
+	int i = 0;
+
+	while (i < n)
+	{
+		q[i] = *(a + n - i -1);
+		i++;
+	}
+}
+
