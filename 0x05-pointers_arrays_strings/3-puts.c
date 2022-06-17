ba2b41017046ac7f4910258b1c71e56109ee9094
victor seyi <tayeski11@gmail.com>
2022-06-17 00:17:07 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * _puts - print string include a newline
+ * @str: string to print
+ * Description: Used to print to screen
+ */
+
+
+void _puts(char *str)
+{
+	 puts(str);
+}
+
